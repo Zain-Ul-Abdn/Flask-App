bd2b35b7cd011d3ec7f19b4191a416457b5f836e
Zain_@Developer <zainmehboob376@gmail.com>
2024-06-10 06:16:54 +0500

    Final Version

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 import re
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
+import pickle
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 
-tokenizer = Tokenizer(num_words=5000)
-loaded_model = load_model('unethical_content_model.h5')
-
+# Function to preprocess text
 def preprocess_text(text):
     text = re.sub(r'\n', ' ', text)
     text = re.sub(r'\r', ' ', text)
@@ -13,14 +12,22 @@ def preprocess_text(text):
     text = text.lower()
     return text
 
+# Function to preprocess input sentence and make prediction
 def is_unethical(sentence):
+    # Load the tokenizer
+    with open('tokenizer.pkl', 'rb') as f:
+        tokenizer = pickle.load(f)
+
     # Preprocess the sentence
     sentence = preprocess_text(sentence)
     sentence_seq = tokenizer.texts_to_sequences([sentence])
     sentence_pad = pad_sequences(sentence_seq, maxlen=100, padding='post')
 
+    # Load the trained model
+    model = load_model('unethical_content_model.h5')
+
     # Predict
-    prediction = loaded_model.predict(sentence_pad)
-    return prediction[0][0] > 0.5
+    prediction = model.predict(sentence_pad)
+    return prediction[0][0] >= 0.5
 
 
