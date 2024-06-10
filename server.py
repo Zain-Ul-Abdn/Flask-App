095b8436426003065f4cc36f042a07cbed87e0d1
Zain_@Developer <zainmehboob376@gmail.com>
2024-06-10 07:35:27 +0500

    Adding cors headers

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, jsonify
-app = Flask("my-app")
-
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 from model import is_unethical
 
-@app.route('/', methods = ['POST']) 
-def index(): 
-   data = request.get_json()
-   input = data.get("data")
-   try:
-       res = is_unethical(input)
-       return jsonify({'response': str(res)})
-   except:
-        return jsonify({'response': "Something went wrong"})
+app = Flask("my-app")
+CORS(app)
+
+@app.route('/', methods=['OPTIONS', 'POST'])
+def index():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+    if request.method == 'POST':
+        data = request.get_json()
+        input = data.get("data")
+        try:
+            res = is_unethical(input)
+            return jsonify({'response': str(res)})
+        except Exception as e:
+            return jsonify({'response': "Something went wrong", 'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
